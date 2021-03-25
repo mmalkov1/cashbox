730d8fce40203dcf4eb6289f068469521b716a00
mmalkov1 <malkov10@yandex.ru>
2021-03-25 23:39:45 +0200

    Update connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('cashbox', 'mmalkov', 'mmalkovcb3fs', {
-  host: process.env.DB_URL,
-  dialect: 'mysql'
-});
-//'cashbox.cwgwhjyresgq.us-east-2.rds.amazonaws.com'
-// const db = new Sequelize('cashbox', 'cashusr', 'ohG846f734gyew', {
-//   host: '185.156.41.138',
+// const db = new Sequelize('cashbox', 'mmalkov', 'mmalkovcb3fs', {
+//   host: process.env.DB_URL,
 //   dialect: 'mysql'
 // });
+//'cashbox.cwgwhjyresgq.us-east-2.rds.amazonaws.com'
+const db = new Sequelize('cashbox', 'cashusr', 'ohG846f734gyew', {
+  host: 'mysql',
+  dialect: 'mysql'
+});
 
 module.exports = db;
